49b4f74e33a8f2f8f811ce1c7aee464ed840b556
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-14 04:30:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday_name, supplies|
       supplies.include?("BBQ") ? holiday_name : nil
-    end.flatten
+    end.flatten.compact
   end
 end
