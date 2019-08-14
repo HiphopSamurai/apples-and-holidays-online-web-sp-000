4da0299644466254d3a533470aefcf2c8260ffb8
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-14 04:19:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
-      puts " #{holiday_name.to_s.split( pattern"_").collect {|split_name|
+      puts " #{holiday_name.to_s.split( pattern "_").collect {|split_name|
   split_name.capitalize}.join(separtor" ")}: #{supplies.join(", ")}"
     end
   end
